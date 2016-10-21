5e9d4aa0ee6de829494755a51bc586e98328aabc
engthiago <t.almeidaeng@gmail.com>
2016-10-20 22:55:54 -0300

    Heroku setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/js'));
 
@@ -7,4 +8,4 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.listen(port);
